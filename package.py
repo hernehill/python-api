01721a01e0d9992d525e803ff3b1a7463d23872d
Terra Jentsch <terra.jentsch@domain.hernehill.com>
2026-07-02 09:44:42 -0500

    Add src dir to PATH

--- package.py
+++ package.py
@@ -24,6 +24,7 @@ variants = [
 def commands():
     env.REZ_SG_API_ROOT = '{root}'
     env.PYTHONPATH.append('{root}/src/shotgun_api3')
+    env.PYTHONPATH.append('{root}/src')
 
 build_command = 'rez python {root}/rez_build.py'
 
